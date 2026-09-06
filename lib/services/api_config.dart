9677689dc14ce4096b8bc140b7658c54ad730b19
JoshiSankar-4021 <joshisankar5051@gmail.com>
2026-09-06 19:10:52 +0530

    Update backend API baseUrl to Vercel deployment

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -3,5 +3,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class ApiConfig {
   static String get baseUrl =>
       dotenv.env['API_BASE_URL'] ??
-      'http://192.168.29.54:3000/api';
+      'https://studenthub-backend-git-main-joshi-sankars-projects.vercel.app/api';
 }
